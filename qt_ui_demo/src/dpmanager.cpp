37070db1242817a61ad2f446059685e373728ac8
yunmo.xu@tuya.com <yunmo.xu@tuya.com>
2022-06-17 15:58:59 +0800

    源文件添加开源声明

--- qt_ui_demo/src/dpmanager.cpp
+++ qt_ui_demo/src/dpmanager.cpp
@@ -1,3 +1,22 @@
+
+/*
+ * Copyright (C) 2021-2022 Symeon XU <xzx_work_2020@163.com>
+ *
+ * QT_UI_DEMO is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QT_UI_DEMO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libQtShadowsocks; see the file LICENSE. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #include "dpmanager.h"
 
 CDpManager::CDpManager(QSharedPointer<CRequestSender> &requestSender, QWidget *parent)
